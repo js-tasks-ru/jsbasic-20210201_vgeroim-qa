a1ac980a85336480512f822223d4e4072d1a6c35
vgeroim <vgeroim.qa@gmail.com>
2021-02-03 19:20:24 -0600

    1-module\1-task solution

--- 1-module/1-task/index.js
+++ 1-module/1-task/index.js
@@ -4,5 +4,29 @@
  * @returns {number}
  */
 function factorial(n) {
-  // ваш код...
+  if(isNumber(n) &&  isNatural(n) ){
+    let result = 1;
+    
+    if (n === 0 || n === 1) {
+      return result;
+    } else {
+        for(i = n; i > 0; i--) {
+            result *= i;
+          }
+      return result;
+    }
+  } else {
+    console.log("Please enter the number greater or equal 0");
+  }
+}
+
+
+//Function validates numbers
+function isNumber(n){
+  return (typeof n === 'number' ? true : false);
+}
+
+//Function validates natural numbers
+function isNatural(n) {
+  return (n >= 0 ? true : false);
 }
