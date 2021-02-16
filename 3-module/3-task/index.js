184a5ae6713e25961507415d835402ad4b967c6c
javascriptru <67475495+jsru-1@users.noreply.github.com>
2021-02-16 20:28:11 +0300

    Added task Переведите текст вида border-left-width в borderLeftWidth

--- 3-module/3-task/index.js
+++ 3-module/3-task/index.js
@@ -1,16 +1,3 @@
 function camelize(str) {
-  let arr = str.split('-'); //split to array
-  let arrCamelCase = [];
-  
-  if (arr[0] === '-') {
-      arrCamelCase = arrayToUpperCase(arr.slice(1)); //all to upper case
-  } else {
-    arrCamelCase = arrayToUpperCase(arr.slice(1)); //all to upper case without first
-    arrCamelCase.unshift(arr[0]); //add first to resutl
-  }
-  return arrCamelCase.join('');
-}
-
-function arrayToUpperCase(arr) {
-  return arr.map(element => element[0].toUpperCase() + element.slice(1));
+  // ваш код...
 }
