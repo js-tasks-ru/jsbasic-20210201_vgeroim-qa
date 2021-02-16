f050bc51b7cdec9200c08af139bca99a110294ac
vgeroim <vgeroim.qa@gmail.com>
2021-02-16 15:19:52 -0600

    3-module/5-task

--- 3-module/5-task/index.js
+++ 3-module/5-task/index.js
@@ -1,3 +1,10 @@
 function getMinMax(str) {
-  // ваш код...
+  let charNumbers = str.split(','); // get rid of all commas
+  charNumbers = charNumbers.join(' ');
+  charNumbers = charNumbers.split(' '); // get rid of all spaces
+
+  let numbers = charNumbers.map(element => Number(element))
+                           .filter(element => !isNaN(element));
+
+  return { min: Math.min(...numbers), max: Math.max(...numbers) }
 }
