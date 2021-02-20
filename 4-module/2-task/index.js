a2d570f1eb017dc8227f26f69e5382e5bd735bb5
vgeroim <vgeroim.qa@gmail.com>
2021-02-20 14:26:06 -0600

    4-module/2-task

--- 4-module/2-task/index.js
+++ 4-module/2-task/index.js
@@ -1,3 +1,7 @@
 function makeDiagonalRed(table) {
-  // ваш код...
+  //let tbody = document.querySelector('table');
+
+  for (let tr of table.rows) {
+    tr.cells[tr.rowIndex].style = 'background-color: red';  
+  }
 }
