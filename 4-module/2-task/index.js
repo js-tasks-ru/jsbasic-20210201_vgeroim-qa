84e4a188e393fb07edb4def6416fb83d947811c5
vgeroim <vgeroim.qa@gmail.com>
2021-02-20 14:29:05 -0600

    delete comment

--- 4-module/2-task/index.js
+++ 4-module/2-task/index.js
@@ -1,6 +1,4 @@
 function makeDiagonalRed(table) {
-  //let tbody = document.querySelector('table');
-
   for (let tr of table.rows) {
     tr.cells[tr.rowIndex].style = 'background-color: red';  
   }
