4ad9317f97f89fefd9a1052577e47521578ac49f
vgeroim <vgeroim.qa@gmail.com>
2021-02-23 20:32:06 -0600

    5-module/2-task

--- 5-module/2-task/index.js
+++ 5-module/2-task/index.js
@@ -1,3 +1,10 @@
 function toggleText() {
-  // ваш код...
+  const button = document.querySelector('.toggle-text-button');
+  const text = document.querySelector('#text');
+
+  button.addEventListener('click', () => {
+    const isTextHidden = text.hidden;
+
+    isTextHidden ? text.hidden = false : text.hidden = true;
+  });
 }
